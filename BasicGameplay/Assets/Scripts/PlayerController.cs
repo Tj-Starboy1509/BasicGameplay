c3e3884052a45db0ad152426f6a751ce5f17ea87
Tristan Tricoche <tricochet@roverkids.org>
2024-11-25 14:16:40 -0500

    added player movement and player boundry

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerCntroller : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,10 @@ public class PlayerCntroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10) {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z); 
+        }
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
