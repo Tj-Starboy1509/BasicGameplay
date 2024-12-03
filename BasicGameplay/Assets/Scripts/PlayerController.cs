8468104e77b934042e74227127daf90887ec22ae
Tristan Tricoche <tricochet@roverkids.org>
2024-12-03 13:20:30 -0500

    added prefab folder and script

--- BasicGameplay/Assets/Scripts/PlayerController.cs
+++ BasicGameplay/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerCntroller : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10.0f;
+
+    public GameObject projectileprefab;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
